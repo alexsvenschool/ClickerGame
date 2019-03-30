df4113b33c0bb66698b0bbb5dccfd19ad6a64945
Sven Bergmann <svenbergmann1997@gmail.com>
2019-03-30 17:01:55 +0100

    Aktualisierung pro Frame & PowerUps

---
+++ ClickerGame/PowerUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace ClickerGame
+{    
+    class PowerUp
+    {
+        private readonly Label lblCost;
+        private readonly Label lblCount;
+        public int count;
+        public int baseCost;
+
+        public PowerUp(Label cost, Label count)
+        {
+            lblCost = cost;
+            lblCount = count;
+        }
+
+
+    }
+}
