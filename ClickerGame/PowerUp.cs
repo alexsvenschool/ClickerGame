a976e59d70d88b7534865e969ce39a5e3f1e984f
Sven Bergmann <svenbergmann1997@gmail.com>
2019-03-31 19:03:49 +0200

    logic

--- ClickerGame/PowerUp.cs
+++ ClickerGame/PowerUp.cs
@@ -10,16 +10,45 @@ namespace ClickerGame
     class PowerUp
     {
         private readonly Label lblCost;
-        private readonly Label lblCount;
-        public int count;
-        public int baseCost;
+        private readonly Label lblCount;        
+        private readonly int baseCost;
+        private int count = 0;
+        private readonly float baseScore;
 
-        public PowerUp(Label cost, Label count)
+        public PowerUp(Label cost, Label count, int bCost, float bScore)
         {
             lblCost = cost;
             lblCount = count;
+            baseCost = bCost;
+            baseScore = bScore;
         }
 
+        public void OnClick()
+        {
+            if(MainWindow.score >= NewCost())
+            {
+                MainWindow.score -= NewCost();
+                MainWindow.scorePerSecond -= ScorePerSecond();
+                count++;
+                MainWindow.scorePerSecond += ScorePerSecond();
+            }
+            
+        }
 
+        public float ScorePerSecond()
+        {
+            return baseScore * count;
+        }
+
+        public void UpdateFrame()
+        {
+            lblCost.Content = NewCost();
+            lblCount.Content = count;
+        }
+
+        public int NewCost()
+        {
+            return (int)(baseCost * Math.Pow(1.08, count));
+        }
     }
 }
